acdd3d4a21954586b92e0876289f815f70b4ef68
N1femi <niffybabs@gmail.com>
2026-09-03 22:31:12 -0400

    Finished barebone gameplayHero

--- src/components/GameplayHero/GameplayHero.jsx
+++ src/components/GameplayHero/GameplayHero.jsx
@@ -2,9 +2,17 @@ import "./GameplayHero.css";
 
 function GameplayHero() {
   return (
-    <section className="gameplay-hero">
-      <p>Gameplay Hero</p>
-    </section>
+    <>
+      <section className="gameplay-hero"> {/* Maybe I can make this mosque with all my skills */}
+        Gameplay Hero, Oluwanifemi Babajide, Driven, Hopeful, Resilient, Curious, Listener, Programmer, Grower, Skilled, Loved
+      </section>
+
+      <section
+        className="gameplay-window"
+        style={{backgroundImage: `url(${"https://placehold.co/200"})` }}
+      >
+      </section>
+    </>
   );
 }
 
