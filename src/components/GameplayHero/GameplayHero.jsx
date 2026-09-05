4d478f6b4d97b8696d78bd3ca300475e56addd25
N1femi <niffybabs@gmail.com>
2026-09-05 14:33:29 -0400

    Finished Marquee Base

--- src/components/GameplayHero/GameplayHero.jsx
+++ src/components/GameplayHero/GameplayHero.jsx
@@ -1,17 +1,49 @@
 import "./GameplayHero.css";
+import cppLogo from "../../assets/icons/cplusplus.svg"
+import pythonLogo from "../../assets/icons/python.svg"
+import reactLogo from "../../assets/icons/react.svg"
+import swiftLogo from "../../assets/icons/swift.svg"
+import typescriptLogo from "../../assets/icons/typescript.svg"
+import javascriptLogo from "../../assets/icons/javascript.svg"
 
 function GameplayHero() {
+  const marqueeItems = [
+    { name: "C++", icon: cppLogo },
+    { name: "Python", icon: pythonLogo },
+    { name: "React", icon: reactLogo },
+    { name: "Swift", icon: swiftLogo },
+    { name: "TypeScript", icon: typescriptLogo },
+    { name: "JavaScript", icon: javascriptLogo },
+  ];
+
   return (
     <>
-      <section className="gameplay-hero"> {/* Maybe I can make this mosque with all my skills */}
-        Gameplay Hero, Oluwanifemi Babajide, Driven, Hopeful, Resilient, Curious, Listener, Programmer, Grower, Skilled, Loved
+      <section className="gameplay-hero">
+        <div className="gameplay-marquee">
+          <div className="marquee-content">
+            {marqueeItems.map((item) => (
+              <span className="marquee-item" key={item.name}>
+                <img src={item.icon} alt="" />
+                {item.name}
+              </span>
+            ))}
+          </div>
+
+          <div className="marquee-content" aria-hidden="true">
+            {marqueeItems.map((item) => (
+              <span className="marquee-item" key={item.name}>
+                <img src={item.icon} alt="" />
+                {item.name}
+              </span>
+            ))}
+          </div>
+        </div>
       </section>
 
       <section
         className="gameplay-window"
-        style={{backgroundImage: `url(${"https://placehold.co/200"})` }}
-      >
-      </section>
+        style={{ backgroundImage: `url("https://placehold.co/200")` }}
+      />
     </>
   );
 }
